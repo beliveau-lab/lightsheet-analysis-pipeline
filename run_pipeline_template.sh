d3d5a77da068cbabc48720f6241a422612c205ae
Nicolas <longnic@uw.edu>
2025-07-17 17:01:52 -0700

    added cellpose 3.0 embedding generation

--- run_pipeline_template.sh
+++ run_pipeline_template.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #$ -N otls_pipeline
-#$ -l mfree=128G
+#$ -l mfree=256G
 #$ -pe serial 1
 #$ -j y
-#$ -o <path_to_log_output>
+#$ -o /net/beliveau/vol1/project/Nico/OTLS/snakemake.log
 #$ -V
 
-
-conda activate <path_to_conda_environment>
-cd <path_to_pipeline_folder>
+conda activate /net/beliveau/vol1/home/longnic/miniconda3/envs/OTLS
+cd /net/beliveau/vol1/project/Nico/OTLS/lightsheet-analysis-pipeline
 
 snakemake --unlock
 snakemake \
@@ -16,4 +15,7 @@ snakemake \
     --cores 1 \
     --latency-wait 60 \
     --use-conda \
-    -p 
+    -p
+
+
+### ALWAYS RUN WITH -n FIRST TO SEE WHICH SNAKEMAKE RULES ARE BEING EXECUTED ###
